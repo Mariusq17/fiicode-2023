4a595111596c783b551e7b5c14e3b9a6211b7f74
Mariusq17 <ignatmarius45@gmail.com>
2023-03-03 16:29:41 +0200

    Am facut side-barul, nerisponsive si fara functii

--- phps/pacienti/home-page.php
+++ phps/pacienti/home-page.php
@@ -5,10 +5,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="/styles/sidebar.css" />
-    <link rel="stylesheet" href="/styles/pacienti/home-page.css" />
+    <link rel="stylesheet" href="../../styles/sidebar.css" />
+    <link rel="stylesheet" href="../../styles/pacienti/home-page.css" />
+    <link rel="stylesheet" href="../../fonts-and-icons/css/all.css">
+    <link rel="stylesheet" href="../../fonts-and-icons/css/all.min.css">
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;700&display=swap" rel="stylesheet">
 </head>
 <body>
-    <?php include "../htmls/sidebar.html" ?>
+    <?php include "../sidebar.php" ?>
 </body>
 </html>
---
+++ phps/sidebar.php
@@ -0,0 +1,59 @@
+<div class="sidebar">
+    <!-- <div class="close-sidebar"></div>
+    <div class="open-sidebar"></div> -->
+    <div class="profile-section">
+      <img
+        src="../../images/user-default-image.png"
+        class="sidebar-user-image"
+      />
+      <div class="sidebar-user-infos">
+        <h3>User FullName</h3>
+        <h4>User Email</h4>
+      </div>
+    </div>
+
+  <div class="sidebar-menu">
+  <a href="#">
+        <div class="sidebar-menu-group">
+        <i class="icon fa-solid fa-house"></i>
+            <h4>Acasa</h4>
+        </div>
+    </a>
+    <a href="#">
+        <div class="sidebar-menu-group">
+        <i class="icon fa-solid fa-user"></i>
+            <h4>Profil</h4>
+        </div>
+    </a>
+    <a href="#">
+        <div class="sidebar-menu-group">
+        <i class="icon fa-regular fa-calendar"></i>
+            <h4>Programarile tale</h4>
+        </div>
+    </a>
+    <a href="#">
+        <div class="sidebar-menu-group">
+        <i class="icon fa-regular fa-comments"></i>
+            <h4>Chat</h4>
+        </div>
+    </a>
+    <a href="#">
+        <div class="sidebar-menu-group">
+        <i class="icon fa-regular fa-clipboard"></i>
+            <h4>Fisa medicala</h4>
+        </div>
+    </a>
+  </div>  
+  <div class="sidebar-log-out-wrapper">
+      <div class="sidebar-log-out-section">
+            <i class="fa-solid fa-moon"></i>
+            <h4>Tema intunecata</h4>
+        </div>
+        <a href="#">
+        <div class="sidebar-log-out-section">
+            <i class="fa-solid fa-arrow-right-from-bracket"></i>
+            <h4>Deconecteza-te</h4>
+        </div>
+      </a>
+  </div>
+</div>
