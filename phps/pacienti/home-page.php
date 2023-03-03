6eb5b6121b2fbd9d7f7fc81ec050ca73a9f55ca8
Mariusq17 <ignatmarius45@gmail.com>
2023-03-03 13:41:09 +0200

    O prima initializare

---
+++ phps/pacienti/home-page.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/styles/sidebar.css" />
+    <link rel="stylesheet" href="/styles/pacienti/home-page.css" />
+</head>
+<body>
+    <?php include "../htmls/sidebar.html" ?>
+</body>
+</html>
